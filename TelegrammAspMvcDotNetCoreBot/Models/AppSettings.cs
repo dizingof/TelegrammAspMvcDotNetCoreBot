ba444a0df7c4bccd62017234a7ccd6147cabd2f1
infopulse\Roman.Nepomniashchyi <Roman.Nepomniashchyi@infopulse.com>
2019-12-19 18:11:46 +0200

    all

--- TelegrammAspMvcDotNetCoreBot/Models/AppSettings.cs
+++ TelegrammAspMvcDotNetCoreBot/Models/AppSettings.cs
@@ -8,7 +8,7 @@ namespace TelegrammAspMvcDotNetCoreBot.Models
     public class AppSettings
     {
         public static string Url { get; set; } = "https://URL:443/{0}";
-        public static string Name { get; set; } = "<BOT_NAME>";
-        public static string Key { get; set; } = "<BOT_KEY>";
+        public static string Name { get; set; } = "@proverka_avto_bot";
+        public static string Key { get; set; } = "780208659:AAGhU8SW47jCy8RDouSrLLAYH-57QScijiM";
     }
 }
