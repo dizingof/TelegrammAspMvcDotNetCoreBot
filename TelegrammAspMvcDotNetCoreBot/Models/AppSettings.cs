8a6173f2db8845da2e7bbf947bdd202e4e6e6da5
dizingof <46020377+dizingof@users.noreply.github.com>
2021-08-31 15:23:16 +0300

    Update AppSettings.cs

--- TelegrammAspMvcDotNetCoreBot/Models/AppSettings.cs
+++ TelegrammAspMvcDotNetCoreBot/Models/AppSettings.cs
@@ -9,6 +9,6 @@ namespace TelegrammAspMvcDotNetCoreBot.Models
     {
         public static string Url { get; set; } = "https://URL:443/{0}";
         public static string Name { get; set; } = "@proverka_avto_bot";
-        public static string Key { get; set; } = "780208659:AAGhU8SW47jCy8RDouSrLLAYH-57QScijiM";
+        public static string Key { get; set; } = "your token";
     }
 }
